1181daa033fead720295c1aba9f7194fe8a0dddf
ejieflorida128 <ejieflorida128@gmail.com>
2024-06-03 11:53:29 +0800

    [ejie]
    changes

--- superAdmin.php
+++ superAdmin.php
@@ -74,7 +74,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link  " href="template/AdminTemplate/pages/tables.html">
+          <a class="nav-link  " href="loadToAdminUserTable.php">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
             <svg width="12px" height="12px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg">
     <circle cx="12" cy="8" r="4"/>
